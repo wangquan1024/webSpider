a891594292dfc23065f2bfcbbc7466bff3ec3b97
wangquan <wangquan2@ygsoft.com>
2019-07-30 14:04:33 +0800

    json demo

--- json/demo3.py
+++ json/demo3.py
@@ -1,1 +1,15 @@
 import json
+
+jsonData = '''{
+    "a": 1,
+    "b": 2
+}
+'''
+
+# json.loads 可以解析json字符串
+# text = json.loads(jsonData)
+# print(text)
+
+with open('obj.json','r',encoding='utf-8') as fp:
+    txt = json.load(fp)
+    print(txt)
